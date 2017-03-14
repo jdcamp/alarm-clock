69540f0c47d178ce04503ae5473f589359e40b54
David Quisenberry and Jake Campa <jake@>
2017-03-14 09:41:08 -0700

    add input for an alarm and format time inputs

--- build/js/app.js
+++ build/js/app.js
@@ -1,7 +1,32 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 
-$(document).ready(function(){
-  $('#time').text(moment());
+$(document).ready(function() {
+    setInterval(function() {
+        $('#time').text(moment().format("hh:mm:ss"));
+    }, 1000);
+
+    var alarm = "9:14:00 am";
+
+    setInterval(function(){
+      var currentTime = $('#time').text();
+      if (currentTime === alarm){
+        $('#alarm').text("RingRIngRIng");
+      }
+    }, 1000);
+
+
+
+    $('#alarm-button').submit(function(event){
+      event.preventDefault();
+      var alarmTime = $('input[name=time]').val();
+      console.log(alarmTime);
+      setInterval(function(){
+        var currentTime = $('#time').text();
+        if (currentTime === alarmTime){
+          $('#alarm').text("RingRIngRIng");
+        }
+      }, 1000);
+    });
 });
 
 },{}]},{},[1]);
--- js/time-interface.js
+++ js/time-interface.js
@@ -1,3 +1,28 @@
-$(document).ready(function(){
-  $('#time').text(moment());
+$(document).ready(function() {
+    setInterval(function() {
+        $('#time').text(moment().format("hh:mm:ss"));
+    }, 1000);
+
+    var alarm = "9:14:00 am";
+
+    setInterval(function(){
+      var currentTime = $('#time').text();
+      if (currentTime === alarm){
+        $('#alarm').text("RingRIngRIng");
+      }
+    }, 1000);
+
+
+
+    $('#alarm-button').submit(function(event){
+      event.preventDefault();
+      var alarmTime = $('input[name=time]').val();
+      console.log(alarmTime);
+      setInterval(function(){
+        var currentTime = $('#time').text();
+        if (currentTime === alarmTime){
+          $('#alarm').text("RingRIngRIng");
+        }
+      }, 1000);
+    });
 });
